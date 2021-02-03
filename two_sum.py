35c1c3d403b598c6b1cc0488cd178f1ba83ce707
Alexandr Kotov <68593244+AlexVines@users.noreply.github.com>
2021-02-04 00:19:11 +0300

    add

---
+++ two_sum.py
@@ -0,0 +1,29 @@
+def twoSum(nums, target: int):
+    """
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+    target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+    >>> twoSum([2,7,11,15], 9)
+    [0, 1]
+    >>> twoSum([3,2,4], 6)
+    [1, 2]
+    >>> twoSum([3,3], 6)
+    [0, 1]
+
+    """
+    counts = dict()
+    for i in range(len(nums)):
+        num = nums[i]
+        remain = target - num
+        if num in counts:
+            return [counts[num], i]
+        else:
+            counts[remain] = i
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
