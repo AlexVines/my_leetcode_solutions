a36bac821c6a93a402f0b7009a87b474da483f49
Alexandr Kotov <68593244+AlexVines@users.noreply.github.com>
2021-02-08 17:54:53 +0300

    add

--- array_partition.py
+++ array_partition.py
@@ -1,8 +1,7 @@
 def arrayPairSum(nums) -> int:
     """
-    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-
-    Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+    Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
+    such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
     >>> arrayPairSum([1,4,3,2])
     4
     >>> arrayPairSum([6,2,6,5,1,2])
